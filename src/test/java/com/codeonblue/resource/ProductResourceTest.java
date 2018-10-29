e9f839fbed8c5ad968d1e3078a474b60a55f9fb4
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2018-10-29 17:34:22 -0300

    Adding ProductResource test for getProductById and deleteProduct APIs

--- src/test/java/com/codeonblue/resource/ProductResourceTest.java
+++ src/test/java/com/codeonblue/resource/ProductResourceTest.java
@@ -3,6 +3,7 @@ package com.codeonblue.resource;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 import org.junit.Test;
@@ -17,13 +18,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.codeonblue.domain.Product;
 import com.codeonblue.repository.ProductRepository;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,7 +35,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(ProductResource.class)
 public class ProductResourceTest {
-
+	
+	private final Product product = createProduct();	
+	private final Optional<Product> optionalProduct = Optional.of(product);
+	
 	@Autowired
 	private MockMvc mockMvc;
 	
@@ -40,7 +47,6 @@ public class ProductResourceTest {
 	
 	@Test
 	public void testShouldGetAllCategories() throws Exception {
-		Product product = createProduct();
 		List<Product> productList = new ArrayList<Product>();
 		productList.add(product);
 						
@@ -56,17 +62,44 @@ public class ProductResourceTest {
 				.andExpect(jsonPath("$[0].price", is(productList.get(0).getPrice().doubleValue())));
 				
 		verify(productRepositoryMock, times(1)).findAll();
-		verifyNoMoreInteractions(productRepositoryMock);
-				
-			
+		verifyNoMoreInteractions(productRepositoryMock);			
 	}
+	
+    @Test
+    public void testShouldGetProductByIdSuccess() throws Exception {
 
-	private Product createProduct() {
-		Random random = new Random();
-		int id = random.nextInt(999);
-		
+        given(productRepositoryMock.findById(1L)).willReturn(optionalProduct);
+        Product productFound = optionalProduct.get();
+
+        mockMvc.perform(get("/products/{id}", product.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+				.andExpect(jsonPath("$.id", is(productFound.getId().intValue())))
+				.andExpect(jsonPath("$.description", is(productFound.getDescription())))
+				.andExpect(jsonPath("$.imageUrl", is(productFound.getImageUrl())))
+				.andExpect(jsonPath("$.price", is(productFound.getPrice().doubleValue())));
+
+        verify(productRepositoryMock, times(1)).findById(product.getId());
+        verifyNoMoreInteractions(productRepositoryMock);
+    }
+    
+    @Test
+    public void testShouldDeleteProductByIdSuccess() throws Exception {
+        Long idToDelete = product.getId();
+        doNothing().when(productRepositoryMock).delete(product);
+        given(productRepositoryMock.findById(idToDelete)).willReturn(optionalProduct);
+
+        mockMvc.perform(delete("/products/{id}", idToDelete))
+                .andExpect(status().isNoContent());
+
+        verify(productRepositoryMock, times(1)).findById(idToDelete);
+        verify(productRepositoryMock, times(1)).delete(product);
+        verifyNoMoreInteractions(productRepositoryMock);
+    }
+	
+	private Product createProduct() {	
 		Product product = new Product();
-		product.setId(new Long(id));
+		product.setId(1L);
 		product.setDescription("Product description");
 		product.setImageUrl("Product image url");
 		product.setPrice(new BigDecimal("10.0"));
